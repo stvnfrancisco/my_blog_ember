a0155ded25ceef1766df328744ec947229e9c037
steven francisco <stvnfrancisco@yahoo.com>
2015-07-07 10:43:54 -0700

    basic front end functionality

---
+++ router.js
@@ -0,0 +1,13 @@
+Blogger.Router.map(function() {
+  this.resource('posts', {path: '/'});
+  this.resource('post', {path: ':post_id'}, function() {
+    this.resource('new-comment');
+  });
+  this.resource('new-post');
+  this.resource('recent-comments');
+  this.resource('about');
+  this.resource('contact', function() {
+    this.resource('email');
+    this.resource('phone');
+  });
+});
