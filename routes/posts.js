b065aa14c356cee68798f289f2b62b9e8868323e
steven francisco <stvnfrancisco@yahoo.com>
2015-07-07 15:27:28 -0700

    update

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,4 @@
 Blogger.PostsRoute = Ember.Route.extend({
-  controllerName: 'posts',
-  renderTemplate: function() {
-    this.render('posts');
-  },
   model: function() {
     return this.store.find('post');
   }
