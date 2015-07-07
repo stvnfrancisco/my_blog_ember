8f2ebbfc9e0938662dbf8d04dc3aa5c5854f9dba
steven francisco <stvnfrancisco@yahoo.com>
2015-07-07 11:51:25 -0700

    pre-lunch push, adds css to background and text

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,8 @@
 Blogger.PostsRoute = Ember.Route.extend({
+  controllerName: 'posts',
+  renderTemplate: function() {
+    this.render('posts');
+  },
   model: function() {
     return this.store.find('post');
   }
