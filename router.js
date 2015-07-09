b8a5baf07ab9beeaff2da1007a4b4ce429cddb66
steven francisco <stvnfrancisco@yahoo.com>
2015-07-09 09:35:30 -0700

    adds new post button back to index page... works properly

--- router.js
+++ router.js
@@ -1,13 +1,14 @@
 Blogger.Router.map(function() {
-  this.resource('posts', {path: '/'});
-  this.resource('post', {path: ':post_id'}, function() {
+  this.resource('posts', {path:'/'});
+  this.resource('about');
+  this.resource('contact', function() {
+    this.resource('phone');
+    this.resource('email');
+  });
+
+  this.resource('recent-comments');
+  this.resource('post', {path:'posts/:post_id'}, function() {
     this.resource('new-comment');
   });
   this.resource('new-post');
-  this.resource('recent-comments');
-  this.resource('about');
-  this.resource('contact', function() {
-    this.resource('email');
-    this.resource('phone');
-  });
 });
